a2bae318c93b791bf2bb6e45effd4ead2941747b
adrien lalut <adrien.lalut@hotmail.fr>
2022-11-15 15:24:52 +0100

    Rails Wikinimous - Half Styled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# TODO: Write a seed to insert 100 posts in the database
+
+puts 'Creating 10 fake posts...'
+10.times do
+  article = Article.new(
+    title: Faker::Movie.title,
+    content: Faker::Movie.quote
+  )
+  article.save!
+end
